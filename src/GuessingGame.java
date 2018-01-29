3797d31c8ab0ae8c97b361a47dd455ec85c0178d
Ravi Akella <ravitejadattaakella@gmail.com>
2018-01-28 16:08:47 -0800

    Restructured code for better readability

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,21 +1,30 @@
-
 import java.util.Scanner;
+
 
 public class GuessingGame {
     int randomNumber;
     int remGuesses;
-    //constructor
-    GuessingGame(){
+    private GameResult gameResult = new GameResult();
+
+    void initializeGame(){
         //initialize game
         System.out.println("Welcome to Guessing Game, starting new Game! In this game, you have 10 chances to guess a number that I-the computer has chosen b/w 1-100\n");
         randomNumber = (int ) (Math.random()*100 + 1);
         remGuesses = 10;
 
     }
+
+    //method getResult
+    GameResult getGameResult() {
+        return gameResult;
+
+    }
     public class GameResult {
         boolean userWon;
         int remGuesses;
         int chosenNumber;
+        boolean gameStatus;
+
 
         GameResult() {
             userWon = false;
@@ -24,54 +33,35 @@ public class GuessingGame {
         }
     }
 
-    GameResult gameResult = new GameResult();
     Scanner scanner = new Scanner(System.in);
 
     //method to start game; illustrating how private class GameResult variables are set using playGame() method only and not exposed outside
-    GameResult playGame(){
+    void playGame() {
         int userGuess;
-        for(remGuesses=10;remGuesses>0;remGuesses--) {
-            System.out.println("You have " + remGuesses + " remaining. Enter your guess\n");
-            userGuess = scanner.nextInt();
+        try {
+            for (remGuesses = 10; remGuesses > 0; remGuesses--) {
+                System.out.println("You have " + remGuesses + " remaining. Enter your guess\n");
+                userGuess = scanner.nextInt();
 
-            if (userGuess > randomNumber) {
-                System.out.println("Its less than " + userGuess + ".");
-            } else if (userGuess < randomNumber) {
-                System.out.println("Its more than " + userGuess + ".");
-            } else {
-                this.gameResult.userWon = true;
-                break;
+                if (userGuess > randomNumber) {
+                    System.out.println("Its less than " + userGuess + ".");
+                } else if (userGuess < randomNumber) {
+                    System.out.println("Its more than " + userGuess + ".");
+                } else {
+                    this.gameResult.userWon = true;
+                    break;
+                }
             }
+            this.gameResult.gameStatus = true;
+            this.gameResult.remGuesses = remGuesses;
+            this.gameResult.chosenNumber = randomNumber;
         }
-        this.gameResult.remGuesses = remGuesses;
-        this.gameResult.chosenNumber = randomNumber;
-
-        return this.gameResult;
+        catch(Exception e) {
+            System.out.println("Error in executing game");
+            this.gameResult.gameStatus = false;
         }
-
-        boolean startGame(){
-        char newGameIp;
-        boolean newGame;
-        playGame();
-        if(this.gameResult.userWon) {
-            System.out.println("Hurray! You guessed it correctly in: " + (11-this.gameResult.remGuesses) + "chances.");
-        }
-        else
-            System.out.println("Oops! You lost, the chosen number is: " + this.gameResult.chosenNumber +".");
-        System.out.println("Do you want to play a new game? Y/N");
-        newGameIp = scanner.next().charAt(0);
-        if(newGameIp == 'y' || newGameIp == 'Y') {
-            newGame = true;
-        }
-        else
-            newGame = false;
-        return newGame;
-        }
-
-
-
-
     }
+}
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        char newGameIp;boolean newGame = true;
         GuessingGame game1 = new GuessingGame();
-        while(game1.startGame()){
-            game1 = new GuessingGame();
-            game1.startGame();
+        GuessingGame.GameResult gameResult;
+        while(newGame){
+            game1.initializeGame();
+            game1.playGame();
+            gameResult = game1.getGameResult();
+            if(gameResult.userWon) {
+                System.out.println("Hurray! You guessed it correctly in: " + (11-gameResult.remGuesses) + "chances.");
+            }
+            else
+                System.out.println("Oops! You lost, the chosen number is: " + gameResult.chosenNumber +".");
+            //check for new game request
+            System.out.println("Do you want to play a new game? Y/N");
+            newGameIp = scanner.next().charAt(0);
+            if(newGameIp == 'y' || newGameIp == 'Y') {
+                newGame = true;
+            }
+            else
+                newGame = false;
         }
-        System.out.println("Thank you for playing Guessing Game, hope you enjoyed it!");
+
     }
 }
